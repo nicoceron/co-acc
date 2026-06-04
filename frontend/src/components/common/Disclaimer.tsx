d884ddf0f92d8cbcaa1c3b848568635310852322
nicoceron <nicocerond@gmail.com>
2026-06-04 14:11:21 -0500

    refactor(frontend): keep only atlas app

--- frontend/src/components/common/Disclaimer.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-import styles from "./Disclaimer.module.css";
-
-export function Disclaimer() {
-  const { t } = useTranslation();
-
-  return <p className={styles.disclaimer}>{t("app.disclaimer")}</p>;
-}
